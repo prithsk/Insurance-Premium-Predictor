66a4597492fbe0603826710e07dcc9bd4bc77be5
Prithvi Krishnan <167022775+prithsk@users.noreply.github.com>
2024-08-27 18:10:41 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,29 +3,8 @@ import pandas as pd
 st.title("Insurance Premium Predictor")
 st.header("By: Prithvi S Krishnan")
 st.subheader("This is the best way to learn about your insurance premium costs after a procedure!  ")
-st.caption("This is an example of how your predicted charges are calculated ")
-st.slider('Slide me', min_value=0, max_value=10)
-st.latex(r''' 1126*1331=1,500,000 ''')
-bar = st.progress(12)
-bar.progress(100)
-st.radio('Region:', ['North','South', 'East', 'West'])
-st.write("THE GRAPH")
-df = pd.DataFrame({
-    'column 1' : [0,1,2,3,4,5,6],
-    'column 2' : [50,100,150,200,250,300,350],
- })
-st.line_chart(df)
-st.multiselect('Multiselect', [1,2,3])
-st.select_slider('Slide to select', options=[1,'2'])
-st.text_input('Enter some text')
-st.number_input('Enter a number')
-st.text_area('Area for textual entry')
-st.date_input('Date input')
-st.time_input('Time entry')
-st.file_uploader('File uploader')
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
+
+
 st.image('insured.jpg')
 
 from joblib import load
